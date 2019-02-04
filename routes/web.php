c09e19000d15f7727dddda69602344d79696b45e
otu591 <45810547+otu591@users.noreply.github.com>
2019-02-05 02:31:11 +0400

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+  
+   
+    Route::get('/', ['as'=>'home','uses'=>'IndexController@show']);
+    Route::get('/weather', ['as'=>'weather','uses'=>'WeatherController@show']);
+    
+    Route::group(['middleware'=>'web'],function (){
+        Route::get('/orders', ['uses'=>'ordersController@show','as'=>'orders',]);
+       
+    
+        Route::get('/editOrder/{id}',['uses'=>'editOrderController@show', 'as'=>'editOrder']);//
+        Route::post('/editOrder/{id}', ['uses'=>'editOrderController@store', 'as'=>'editOrder']);
+    });
